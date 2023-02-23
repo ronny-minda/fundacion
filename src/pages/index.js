68e0cac5e12a905929c9e01da9ea5f7bdf592f95
ronny-minda <ronny.minda.vera@gmail.com>
2023-02-22 18:32:57 -0600

    prototipo

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,9 @@ export default function Home() {
     <>
 
       <Head>
+        <title>Fundacion Rodiv</title>
         <link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all"></link>
+        <link rel="icon" href="/favicon.png" type="image/png" />
         <script src="/layout/scripts/jquery.min.js"></script>
         <script src="/layout/scripts/jquery.backtotop.js"></script>
         <script src="/layout/scripts/jquery.mobilemenu.js"></script>
